238ae7a0e99d825f0f5df2347d0086dc2867ece4
Chris Donati <chris.donati@appscale.com>
2018-04-18 01:09:10 -0700

    Move entity handling work to separate module

--- factoring_service/factor.py
+++ factoring_service/factor.py
@@ -1,26 +1,14 @@
 import webapp2
-from datetime import date, datetime
 from sympy.ntheory import factorint
-from google.appengine.ext import ndb
 
-class Factors(ndb.Model): # id = number to be factored
-    request_ts = ndb.DateTimeProperty(auto_now_add=True)
-    result_ts = ndb.DateTimeProperty()
-    result_msg = ndb.StringProperty()
-    result_list = ndb.StringProperty(repeated=True)
+from factors import Factors
+
 
 class DoFactor(webapp2.RequestHandler):
     def post(self):
-        number = self.request.get('number')
-        f = Factors.get_by_id(number)
-
-        f.result_list = []
-        factors = factorint(int(number)) # do the calculation
-        for k in factors.keys():
-            f.result_list.append(str(k))
-        f.result_msg = "computed"
-        f.result_ts = datetime.now()
-        f.put()
+        number = int(self.request.get('number'))
+        factors = factorint(number) # do the calculation
+        Factors.set_result(number, factors.keys())
 
 app = webapp2.WSGIApplication([
     ('/factor', DoFactor),
--- main.py
+++ main.py
@@ -1,56 +1,32 @@
-import webapp2
 import json
-from datetime import date, datetime
-from google.appengine.ext import ndb
-from google.appengine.api import taskqueue
 
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
+import webapp2
 
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    raise TypeError ("Type %s not serializable" % type(obj))
+from factoring_service.factors import Factors
 
-def write_response(response, list):
-    reply = json.dumps(list, indent=4, default=json_serial)
-    response.headers['Content-Type'] = 'text/json'
-    response.write(reply)
-        
-class Factors(ndb.Model): # id = number to be factored
-    request_ts = ndb.DateTimeProperty(auto_now_add=True)
-    result_ts = ndb.DateTimeProperty()
-    result_msg = ndb.StringProperty()
-    result_list = ndb.StringProperty(repeated=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Welcome to factapp!')
 
+
 class ListFactors(webapp2.RequestHandler):
     def get(self):
         offset = int(self.request.get("offset", default_value=0))
         limit = int(self.request.get("limit", default_value=10))
+        results = [result.safe_dict()
+                   for result in Factors.list(offset, limit)]
+        self.response.headers['Content-Type'] = 'text/json'
+        json.dump(results, self.response, indent=4)
 
-        q = Factors.query().order(-Factors.request_ts).fetch(offset=offset, limit=limit)
-        list = [ ]
-        for p in q: 
-            list.append(dict(p.to_dict(), number=p.key.id()))
-        write_response(self.response, list)
 
 class GetFactor(webapp2.RequestHandler):
     def get(self, number):
-        f = Factors.get_or_insert(number)
-        if f.result_msg is None: # number not in DB
-            f.result_msg = "pending"
-            f.put()
-            task = taskqueue.add( # check for queue errors?
-                url = "/factor",
-                target = "factoring-service",
-                params = { 'number': number })
+        result = Factors.get_or_start_factoring(number)
+        self.response.headers['Content-Type'] = 'text/json'
+        json.dump(result.safe_dict(), self.response, indent=4)
 
-        list = [ dict(f.to_dict(), number=number) ]
-        write_response(self.response, list)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
