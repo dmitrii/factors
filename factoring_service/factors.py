f31099d0eca93b8547b61a9bde64f7f4c2c06e0f
Chris Donati <chris.donati@appscale.com>
2018-04-18 21:23:40 -0700

    Add additional queries
    This also uses memcache to cache the query by request timestamp.

--- factoring_service/factors.py
+++ factoring_service/factors.py
@@ -1,8 +1,14 @@
 """ Represents the results of a prime factorization. """
+from __future__ import division
+
 from datetime import datetime
 
+from google.appengine.api import memcache
 from google.appengine.api import taskqueue
 from google.appengine.ext import ndb
+
+# The number of seconds to keep queries cached.
+QUERY_CACHE_TTL = 10
 
 
 class Factors(ndb.Model):
@@ -11,8 +17,8 @@ class Factors(ndb.Model):
     result_ts = ndb.DateTimeProperty(indexed=False)
     result_msg = ndb.StringProperty(default='pending', indexed=False)
     result_list = ndb.BlobProperty(repeated=True, indexed=False)
-#    compute_duration = ndb.ComputedProperty(lambda self: (self.result_ts - self.request_ts).seconds)
-    result_list_size = ndb.ComputedProperty(lambda self: len(self.result_list))
+    compute_duration = ndb.FloatProperty()
+    result_list_size = ndb.IntegerProperty()
 
     @staticmethod
     @ndb.transactional
@@ -33,21 +39,50 @@ class Factors(ndb.Model):
 
     @staticmethod
     def list(offset, limit):
-        query = Factors.query().order(-Factors.request_ts)
+        cache_key = ':'.join(['-request_ts', str(offset), str(limit)])
+        results = memcache.get(cache_key)
+        if results is None:
+            query = Factors.query().order(-Factors.request_ts)
+            results = query.fetch(offset=offset, limit=limit)
+            memcache.add(cache_key, results, QUERY_CACHE_TTL)
 
-        # query = Factors.query()
-        # SELECT * FROM Factors ORDER BY __key__ DESC
-        # SELECT * FROM Factors ORDER BY compute_duration
+        return results
 
-        return query.fetch(offset=offset, limit=limit)
+    @staticmethod
+    def list_by_compute_duration(offset, limit):
+        return Factors.gql(
+            'ORDER BY compute_duration DESC '
+            'LIMIT {} '
+            'OFFSET {}'.format(limit, offset)
+        )
+
+    @staticmethod
+    def list_by_result_size(offset, limit):
+        return Factors.gql(
+            'ORDER BY result_list_size DESC '
+            'LIMIT {} '
+            'OFFSET {}'.format(limit, offset)
+        )
+
+    @staticmethod
+    def list_by_key(offset, limit):
+        return Factors.gql(
+            'ORDER BY __key__ '
+            'LIMIT {} '
+            'OFFSET {}'.format(limit, offset)
+        )
 
     @staticmethod
     def set_result(product, result_list):
         key = ndb.Key(Factors, str(product))
+        current_time = datetime.utcnow()
         factors = key.get()
         factors.result_list = [str(result) for result in result_list]
         factors.result_msg = 'computed'
-        factors.result_ts = datetime.utcnow()
+        factors.result_ts = current_time
+        duration = current_time - factors.request_ts
+        factors.compute_duration = duration.microseconds / 1000000
+        factors.result_list_size = len(factors.result_list)
         factors.put()
 
     @staticmethod
