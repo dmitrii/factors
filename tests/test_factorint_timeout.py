e3c94957f0d3a14d567352f61c0794393267959d
Dmitrii Calzago <dcalzago@gmail.com>
2018-04-20 12:43:12 -0700

    testing timeout with signal

--- tests/test_factorint_timeout.py
+++ tests/test_factorint_timeout.py
@@ -1,19 +1,14 @@
-import multiprocessing
 from sympy.ntheory import factorint
+import signal
 
-def worker():
-    factorint(1239458712349817098779081798721340987234987987234, verbose=True)
+def sig_handler(signum, frame):
+    print "caught signal"
+    raise Exception("timeout")
 
 if __name__ == '__main__':
-    p = multiprocessing.Process(target=worker)
-    p.start()
-
-    # wait for 5 seconds or until process finishes
-    p.join(5)
-
-    if p.is_alive():
-        print "still running... terminate it"
-
-        # Terminate
-        p.terminate()
-        p.join()
+    signal.signal(signal.SIGALRM, sig_handler)
+    signal.alarm(5)
+    try:
+        factorint(1239458712349817098779081798721340987234987987234, verbose=True)
+    except Exception, exc:
+        print exc
