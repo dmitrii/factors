0d1946cd270b37f3b7960714dc69893c871f8df9
Dmitrii Calzago <dcalzago@gmail.com>
2018-04-24 23:59:18 -0700

    exploring factorint() limits

--- tests/test_factorint_timeout.py
+++ tests/test_factorint_timeout.py
@@ -1,14 +1,22 @@
 from sympy.ntheory import factorint
 import signal
+import time
 
 def sig_handler(signum, frame):
     print "caught signal"
     raise Exception("timeout")
 
 if __name__ == '__main__':
+    N = 1239458712349817098779081798721340987234987987234
+
     signal.signal(signal.SIGALRM, sig_handler)
-    signal.alarm(5)
-    try:
-        factorint(1239458712349817098779081798721340987234987987234, verbose=True)
-    except Exception, exc:
-        print exc
+    l = 1
+    while l <= 10000000: # at 10M, the big numbers seem to take longer than 2min and more than 3GB of memory
+        signal.alarm(120) # cancels previous alarm
+        start_time = time.time()
+        try:
+            factors = factorint(N, limit=l, verbose=False)
+        except Exception, exc:
+            print exc
+        print "limit=" + str(l) + " elapsed: " + str(time.time() - start_time) + " result: " + str(factors)
+        l *= 10
